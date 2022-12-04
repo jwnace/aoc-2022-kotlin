403f67816c0227a705510594b60a500e8f964924
Joe Nace <jwnace@gmail.com>
2022-12-03 20:03:59 -0500

    added placeholder for day 2 solution

---
+++ src/main/kotlin/day02/day02.kt
@@ -0,0 +1,14 @@
+package day02
+
+import java.io.File
+
+fun main() {
+    val elves = File("src/main/kotlin/day02/day02.txt").readLines()
+
+    fun part1() = 1
+
+    fun part2() = 2
+
+    println(part1())
+    println(part2())
+}
