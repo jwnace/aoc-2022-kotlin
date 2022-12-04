94789818c8379c38d0edb6de4b0bcf69a41a6d4c
Joe Nace <jwnace@gmail.com>
2022-12-04 14:35:55 -0500

    added solution for day 3

--- src/main/kotlin/day03/day03.kt
+++ src/main/kotlin/day03/day03.kt
@@ -5,9 +5,15 @@ import java.io.File
 fun main() {
     val input = File("src/main/kotlin/day03/day03.txt").readLines()
 
-    fun part1() = 1
+    fun getPriority(letter: Char) = if (letter.isLowerCase()) letter - 'a' + 1 else letter - 'A' + 27
 
-    fun part2() = 2
+    fun part1() = input.map { it.chunked(it.length / 2) }
+        .map { (left, right) -> (left.toSet() intersect right.toSet()).single() }
+        .sumOf { getPriority(it) }
+
+    fun part2() = input.chunked(3)
+        .map { (a, b, c) -> (a.toSet() intersect b.toSet() intersect c.toSet()).single() }
+        .sumOf { getPriority(it) }
 
     println(part1())
     println(part2())
