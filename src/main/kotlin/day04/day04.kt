41de31598b216774e3ae61989d769556ab1eb04f
Joe Nace <jwnace@gmail.com>
2022-12-04 14:50:46 -0500

    added placeholder for day 4 solution

---
+++ src/main/kotlin/day04/day04.kt
@@ -0,0 +1,14 @@
+package day04
+
+import java.io.File
+
+fun main() {
+    val input = File("src/main/kotlin/day04/day04.txt").readLines()
+
+    fun part1() = 1
+
+    fun part2() = 2
+
+    println(part1())
+    println(part2())
+}
